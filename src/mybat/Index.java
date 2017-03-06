d1edeaba70a60171c07916e4d55cd1ac85573fcd
tkillman <dkkim1004@hanmail.net>
2017-03-06 16:29:42 +0900

    <myBatis 처음>

---
+++ src/mybat/Index.java
@@ -0,0 +1,36 @@
+package mybat;
+
+public class Index {
+
+	/*
+	 * xml 파일 - 환경설정, mapper 파일(SQL)
+	 * 
+	 * SqlSessionFactoryBuilder.build()
+	 * SqlSessionFactory.openSession();
+	 * SqlSession
+	 * 
+	 * 
+	 * insert()
+	 * update()
+	 * delete()
+	 * 
+	 * selectOne()
+	 * selectList()
+	 * 
+	 * close() <- 항상 세션을 닫자.
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * */
+	
+	
+	
+	
+	
+	
+}
