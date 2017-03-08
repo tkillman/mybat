99b7f0961be2043d973a1b871b4ff41b3ea48e80
tkillman <dkkim1004@hanmail.net>
2017-03-08 14:02:23 +0900

    <myBatis 동적쿼리 정리>

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -6,51 +6,76 @@ import java.util.Map;
 
 import member.DAO.Dao;
 import member.DTO.MemDto;
+import member.DTO.Name;
 
 public class Main {
-		static Dao dao = new Dao();
-	
+	static Dao dao = new Dao();
+	static List<MemDto> list = null;
+
 	public static void main(String[] args) {
-		
-		//selectList()
-		
-		
-		List<MemDto> list = dao.selectAll();
-		
-		for(MemDto memberDto: list){
-			
-			System.out.println(memberDto);
-			
-		}
+
+		// 1.selectList()
 		/*
-		//insert()
-		
-		MemDto memDto = new MemDto();
-		memDto.setMemberId("김");
-		memDto.setPassword("가");
-		memDto.setName("gkgk");
-		memDto.setEmail("아");
-		
-		dao.insert(memDto);
-		
-		
-		//delete()
-		int a =dao.delete("a");
-		*/
-		
-		//update()
-		
-		/*Map<String, String> map = new HashMap<>();
-		map.put("memberId", "a");
-		map.put("password", "123");
-		
-		int a = dao.update(map);
-		
-		System.out.println(a);*/
-		
-		
-		
-	}
-	
-	
-}
+		 * list = dao.selectAll();
+		 * 
+		 * for(MemDto memberDto: list){ System.out.println(memberDto); }
+		 */
+
+		/*
+		 * //2.insert() MemDto memDto = new MemDto(); memDto.setMemberId("김");
+		 * memDto.setPassword("가"); memDto.setName("gkgk");
+		 * memDto.setEmail("아");
+		 * 
+		 * dao.insert(memDto);
+		 * 
+		 * 
+		 * //3.delete() int a =dao.delete("a");
+		 */
+
+		// 4.update()
+
+		/*
+		 * Map<String, String> map = new HashMap<>(); map.put("memberId", "a");
+		 * map.put("password", "123");
+		 * 
+		 * int a = dao.update(map);
+		 * 
+		 * System.out.println(a);
+		 */
+
+		// 5.useIf()
+
+		/*
+		 * Name name = new Name(); name.setName("kimdong"); list =
+		 * dao.useIf(name); for(MemDto memberDto: list){
+		 * System.out.println(memberDto); }
+		 */
+
+		// 6.ModifyUserSetIf()
+		/*
+		 * MemDto memDto = new MemDto(); memDto.setMemberId("tk");
+		 * memDto.setPassword("1232"); memDto.setName("modi");
+		 * memDto.setEmail("modify@naver.com");
+		 * 
+		 * int resultNum =dao.modifyUseSetIf(memDto);
+		 * System.out.println(resultNum);
+		 */
+
+		// 7. useForeach()
+
+		/*
+		 * String[] strs = {"modi","kimdong"}; list = dao.useForeach(strs);
+		 * 
+		 * for(MemDto memberDto: list){ System.out.println(memberDto); }
+		 */
+
+		// 8.useLike
+
+		String str = "naver";
+		list = dao.useLike(str);
+		for (MemDto memberDto : list) {
+			System.out.println(memberDto);
+		}
+
+	}// 메인 메소드 종료
+} // 클래스 종료
--- src/member/DAO/Dao.java
+++ src/member/DAO/Dao.java
@@ -1,67 +1,119 @@
 package member.DAO;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 
 import member.DTO.MemDto;
+import member.DTO.Name;
 
 public class Dao {
-	
-	List<MemDto> list ;
-	
-	
-	public List<MemDto> selectAll(){
-		
-		//쿼리를 실행시킬 수 있는 메소드를 가지고 있다.
-			SqlSession sqlSession =MakeSqlSessionFactory.factory.openSession();	
-		
-		// 매개변수로  mapper namespace . select id값을 넣어준다. 
-		list=sqlSession.selectList("member.getlist");
-		
+
+	List<MemDto> list;
+
+	// selectList()
+	public List<MemDto> selectAll() {
+
+		// 쿼리를 실행시킬 수 있는 메소드를 가지고 있다.
+		SqlSession sqlSession = MakeSqlSessionFactory.factory.openSession();
+
+		// 매개변수로 mapper namespace . select id값을 넣어준다.
+		list = sqlSession.selectList("member.getlist");
+
 		sqlSession.close();
+
+		return list;
+	}
+
+	//insert()
+	public void insert(MemDto memDto) {
+
+		SqlSession sqlSession = MakeSqlSessionFactory.factory.openSession();
+
+		sqlSession.insert("member.add", memDto);
+		sqlSession.commit();
+
+		sqlSession.close();
+
+	}
+
+	// delete()
+	public int delete(String memberId) {
+		int resultNum = -1;
+		SqlSession sqlSession = MakeSqlSessionFactory.factory.openSession();
+
+		resultNum = sqlSession.delete("member.del", "a");
+		sqlSession.commit();
+		sqlSession.close();
+
+		return resultNum;
+	}
+
+	// update()
+	public int update(Map map) {
+		int resultNum = -1;
+		SqlSession sqlSession = MakeSqlSessionFactory.factory.openSession();
+
+		resultNum = sqlSession.update("member.Modify", map);
+		sqlSession.commit();
+		sqlSession.close();
+
+		return resultNum;
+	}
+	
+	//useIf
+	public List<MemDto> useIf(Name name) {
+
+		// 쿼리를 실행시킬 수 있는 메소드를 가지고 있다.
+		SqlSession sqlSession = MakeSqlSessionFactory.factory.openSession();
+
 		
+		list = sqlSession.selectList("member.useIf",name);
+
+		sqlSession.close();
+
 		return list;
 	}
 	
-		public void  insert(MemDto memDto){
+	// ModifyUseSetIf
+		public int modifyUseSetIf(MemDto memDto) {
+			int resultNum = -1;
+			SqlSession sqlSession = MakeSqlSessionFactory.factory.openSession();
+
+			resultNum = sqlSession.update("member.ModifyUseSetIf", memDto);
+			sqlSession.commit();
+			sqlSession.close();
+
+			return resultNum;
+		}
+
+		// useForeach()
+		public List<MemDto> useForeach(String[] strs) {
+
+			// 쿼리를 실행시킬 수 있는 메소드를 가지고 있다.
+			SqlSession sqlSession = MakeSqlSessionFactory.factory.openSession();
+
+			// 매개변수로 mapper namespace . select id값을 넣어준다.
+			list = sqlSession.selectList("member.useForeach", strs);
+
+			sqlSession.close();
+
+			return list;
+		}
 		
-		SqlSession sqlSession =  MakeSqlSessionFactory.factory.openSession();
-		
-		
-		
-		sqlSession.insert("member.add", memDto);
-		sqlSession.commit();
-		
-		sqlSession.close();
-	
-	
-	}
-	
-	public int  delete(String memberId){
-		int resultNum =-1;
-		SqlSession sqlSession =  MakeSqlSessionFactory.factory.openSession();
-		
-		resultNum =sqlSession.delete("member.del", "a");
-		sqlSession.commit();
-		sqlSession.close();
-	
-		return resultNum;
-	}
-	
-	public int update(Map map){
-		int resultNum =-1;
-		SqlSession sqlSession =  MakeSqlSessionFactory.factory.openSession();
-	
-		
-		resultNum = sqlSession.update("member.Modify",map);
-		sqlSession.commit();
-		sqlSession.close();
-	
-		return resultNum;
-	}
-	
-	
+		//useLike
+		public List<MemDto> useLike(String str) {
+
+			// 쿼리를 실행시킬 수 있는 메소드를 가지고 있다.
+			SqlSession sqlSession = MakeSqlSessionFactory.factory.openSession();
+
+			// 매개변수로 mapper namespace . select id값을 넣어준다.
+			list = sqlSession.selectList("member.useLike", str);
+
+			sqlSession.close();
+
+			return list;
+		}
+
 }
